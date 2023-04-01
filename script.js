6ccbd68f7dfc35f5b08a78c187a66cef790aee23
Ravi Singh <ravi4636singh@gmail.com>
2023-04-02 00:30:39 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('button')
+let resultScreen = document.querySelector('#screen')
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        if(e.target.textContent === 'AC'){
+            resultScreen.value = ''
+        }else if(e.target.textContent === 'C'){
+            resultScreen.value = resultScreen.value.toString().slice(0, -1)
+        }else if(e.target.textContent === '='){
+            if(resultScreen.value === ''){
+                resultScreen.value = ''
+            }else{
+                let ans = eval(resultScreen.value)
+                resultScreen.value = ans
+            }
+        }else{
+            resultScreen.value += e.target.textContent
+        }
+        
+    })
+})
